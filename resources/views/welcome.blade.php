5afa996f72e048f07c543be42d82a01d44d16462
Deepak Kumar <deepak@whyable.com>
2017-04-10 21:40:02 -0400

    W3C validation fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>DWA A3</title>
+        <title>Laravel Bill Splitter</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <!-- Compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://bootswatch.com/spacelab/bootstrap.min.css">
-        <title>Assignment 3 - Bill Splitter</title>
+
 
         <!-- Styles -->
         <style>
